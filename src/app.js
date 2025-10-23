8df37f5d6426075cfb9911a5b502cb86b6197b3c
Pablo Sosa <sosadavid4280@gmail.com>
2025-10-23 11:21:34 -0600

    feat: agregar swagger

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import swaggerJSDoc from "swagger-jsdoc";
+import swagger from "swagger-ui-express";
 import { sequelize } from "./config/database.js";
 import { rutas } from "./rutas/index.js";
+import { swaggerOptions } from "./swagger.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api", rutas);
+app.use("/api/docs", swagger.serve, swagger.setup(swaggerDocs));
+
+app.use((_request, response) => {
+  response.redirect("/api/docs");
+});
 
 try {
   await sequelize.authenticate();
@@ -22,7 +31,3 @@ try {
   console.error("Error: No se pudo iniciar el servidor.");
   console.error(error);
 }
-
-app.use((_request, response) => {
-  response.redirect("/api/docs");
-});
