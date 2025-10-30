20804744d9ca41de287a8f568a0920475a2b6f8f
Pablo Sosa <sosadavid4280@outlook.com>
2025-10-30 14:45:50 -0600

    feat: agregar servicios de correo

---
+++ src/servicios/email.servicio.mjs
@@ -0,0 +1,15 @@
+import { transportador } from '../config/email.config.mjs';
+
+export const correoBienvenida = async (correo, nombre) => {
+  const opciones = { to: correo, subject: `¡Te damos la bienvenida ${nombre}!` };
+
+  try {
+    await transportador.sendMail(opciones);
+    console.log(`Correo de bienvenida enviado a: ${correo}`);
+    return { exito: true };
+  } catch (error) {
+    console.error('Error: No se pudo enviar el correo de bienvenida');
+    console.error(error.message);
+    return { exito: false, error: error.message };
+  }
+};
